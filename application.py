68d6621ccb0094b78b20d51147455d4a39a06107
AStan7 <55993416+AStan7@users.noreply.github.com>
2023-02-01 10:52:11 +0000

    (testing)

--- application.py
+++ application.py
@@ -3,8 +3,67 @@ from flask import Flask, request, render_template
 import pickle
 import numpy as np
 from scipy.spatial import distance
+from pathlib import Path
+import sys
+import os
+import glob
+import re
+from pathlib import Path
+from io import BytesIO
+import base64
+import requests
+import numpy
+import cv2
+from scipy.spatial import distance##########
+import io
+from PIL import Image as PILImage
+
+# Import fast.ai Library
+from fastai import *
+from fastai.vision import *
 
 application = Flask(__name__)
+
+# C:/Users/Aran/Desktop/TUD Work/Year 4/Final Year Project/Code/Testing on Python 3.9/SkinCancerDetection-WebAppNAME_OF_FILE = 'model_best' # Name of your exported file
+NAME_OF_FILE = 'model_best' # Name of your exported file
+PATH_TO_MODELS_DIR = Path('/models') # by default just use /models in root dir
+classes = ['Actinic keratoses', 'Basal cell carcinoma', 'Benign keratosis',
+           'Dermatofibroma', 'Melanocytic nevi', 'Melanoma', 'Vascular lesions']
+
+def setup_model_pth(path_to_pth_file, learner_name_to_load, classes):
+    data = ImageDataBunch.single_from_classes(
+        path_to_pth_file, classes, ds_tfms=get_transforms(), size=224).normalize(imagenet_stats)
+    learn = cnn_learner(data, models.densenet169, model_dir='models')
+    learn.load(learner_name_to_load, device=torch.device('cpu'))
+    return learn
+
+learn = setup_model_pth(PATH_TO_MODELS_DIR, NAME_OF_FILE, classes)
+
+def encode(img):
+    img = (image2np(img.data) * 255).astype('uint8')
+    pil_img = PILImage.fromarray(img)
+    #IP(pil_img)
+    #pil_img = IP(pil_img)
+    #pil_img.show()
+    buff = BytesIO()
+    pil_img.save(buff, format="JPEG")
+    return base64.b64encode(buff.getvalue()).decode("utf-8")
+
+def model_predict(img):
+    img = open_image(BytesIO(img))
+    pred_class,pred_idx,outputs = learn.predict(img)
+    formatted_outputs = ["{:.1f}%".format(value) for value in [x * 100 for x in torch.nn.functional.softmax(outputs, dim=0)]]
+    pred_probs = sorted(
+            zip(learn.data.classes, map(str, formatted_outputs)),
+            key=lambda p: p[1],
+            reverse=True
+        )
+	
+    img_data = encode(img)
+    result = {"class":pred_class, "probs":pred_probs, "image":img_data}
+    return render_template('result.html', result=result)
+
+
 
 @application.route('/')
 def index():
@@ -16,16 +75,16 @@ def test():
     return render_template('test.html')
 
 
-# @application.route('/upload', methods=["POST", "GET"])
-# def upload():
-#     if request.method == 'POST':
-#         # Get the file from post request
-#         img = request.files['file'].read()
+@application.route('/upload', methods=["POST", "GET"])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        img = request.files['file'].read()
 
-#         # Make prediction
-#         preds = model_predict(img)
-#         return preds
-#     return 'OK'
+        # Make prediction
+        preds = model_predict(img)
+        return preds
+    return 'OK'
 
 
 
